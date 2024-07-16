a55ea8b42aa888862c824bf6208e774dd191fa55
Rahul S Kumar <rahulskumar@Rahuls-MacBook-Air.local>
2024-07-16 20:57:32 +0530

    first commit

---
+++ client/src/components/CommentList.js
@@ -0,0 +1,24 @@
+import {useState,useEffect} from 'react';
+import axios from 'axios';
+
+const CommentList = ({postId}) => {
+    const [comments, setComments] = useState({})
+
+    const fetchComments = async () => {
+        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+        setComments(res.data);
+    }
+
+    useEffect(() => {
+        fetchComments();
+    }, [])
+
+    const renderedPost = Object.values(comments).map(post => {
+        return <li key={post.id}>{post.content}</li>
+    })
+  return (
+    <ul>{renderedPost}</ul>
+  )
+}
+
+export default CommentList
