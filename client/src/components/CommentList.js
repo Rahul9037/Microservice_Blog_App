bd844e5feb4ac7b41d3f4e550d26e99807541dc1
Rahul S Kumar <rahulskumar@Rahuls-MacBook-Air.local>
2024-07-17 10:32:34 +0530

    event bus changes

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,17 +1,17 @@
-import {useState,useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 
-const CommentList = ({postId}) => {
-    const [comments, setComments] = useState({})
+const CommentList = ({comments}) => {
+    // const [comments, setComments] = useState({})
 
-    const fetchComments = async () => {
-        const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-        setComments(res.data);
-    }
+    // const fetchComments = async () => {
+    //     const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
+    //     setComments(res.data);
+    // }
 
-    useEffect(() => {
-        fetchComments();
-    }, [])
+    // useEffect(() => {
+    //     fetchComments();
+    // }, [])
 
     const renderedPost = Object.values(comments).map(post => {
         return <li key={post.id}>{post.content}</li>
---
+++ event-bus/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events" , (req,res) => {
+    const event = req.body;
+    axios.post("http://localhost:4000/events" , event).catch(err => console.log(err))
+    axios.post("http://localhost:4001/events" , event).catch(err => console.log(err))
+    axios.post("http://localhost:4002/events" , event).catch(err => console.log(err))
+    res.send({status:"OK"})
+})
+
+app.listen(4005 , () =>{
+    console.log("listening to 4005")
+})
